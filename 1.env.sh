a006dc01de7343ee9aa93f193ec2ca724325f541
pa1kumarbh <38250665+pa1kumarbh@users.noreply.github.com>
2018-11-29 16:35:26 +0530

    update
    updated the path

--- 1.env.sh
+++ 1.env.sh
@@ -1,2 +1,6 @@
 export DOMAIN_NAME=apps.opsday.ocpcloud.com
 export DOCKER_REGISTRY_URL=docker-registry-default.apps.opsday.ocpcloud.com
+
+
+#export DOMAIN_NAME=apps.opsday.ocpcloud.com
+#export DOCKER_REGISTRY_URL=docker-registry-default.apps.opsday.ocpcloud.com
